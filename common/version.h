de3949564078d917104105ce62bf7925eee72a75
Guillaume Meunier <guillaume.meunier@centraliens.net>
2023-11-24 12:00:16 +0100

    Publish server and protocol version on avahi

--- common/version.h
+++ common/version.h
@@ -1,3 +1,13 @@
 #pragma once
 
-extern const char git_version[];
+#include "wivrn_serialization.h"
+#include "wivrn_packets.h"
+
+
+namespace xrt::drivers::wivrn
+{
+	using protocol = std::pair<from_headset::packets, to_headset::packets>;
+
+	extern const char git_version[];
+	static inline const constinit auto protocol_version = serialization_type_hash<protocol>();
+}
