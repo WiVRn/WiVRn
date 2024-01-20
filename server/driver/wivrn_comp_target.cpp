c144f7e1071b2c735c0c9f999ef76fe9fac4461d
Patrick Nicolas <patricknicolas@laposte.net>
2024-01-20 15:05:10 +0100

    Update monado

--- server/driver/wivrn_comp_target.cpp
+++ server/driver/wivrn_comp_target.cpp
@@ -658,9 +658,12 @@ static void comp_wivrn_mark_timing_point(struct comp_target * ct,
 			cn->cnx->dump_time("begin", cn->frame_index + 1, when_ns);
 			u_pc_mark_point(cn->upc, U_TIMING_POINT_BEGIN, cn->current_frame_id, when_ns);
 			break;
-		case COMP_TARGET_TIMING_POINT_SUBMIT:
+		case COMP_TARGET_TIMING_POINT_SUBMIT_BEGIN:
+			u_pc_mark_point(cn->upc, U_TIMING_POINT_SUBMIT_BEGIN, cn->current_frame_id, when_ns);
+			break;
+		case COMP_TARGET_TIMING_POINT_SUBMIT_END:
 			cn->cnx->dump_time("submit", cn->frame_index + 1, when_ns);
-			u_pc_mark_point(cn->upc, U_TIMING_POINT_SUBMIT, cn->current_frame_id, when_ns);
+			u_pc_mark_point(cn->upc, U_TIMING_POINT_SUBMIT_END, cn->current_frame_id, when_ns);
 			break;
 		default:
 			assert(false);
