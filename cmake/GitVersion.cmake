6d808eac940430729ebf472b966c92078d6c9ff7
Guillaume Meunier <guillaume.meunier@centraliens.net>
2024-01-27 23:59:27 +0100

    CI update: - Deep clone to have the correct version number displayed - Split the different APKs in matrix jobs

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -1,10 +1,16 @@
 
-execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always OUTPUT_VARIABLE GIT_DESC ERROR_QUIET)
+execute_process(
+	COMMAND ${GIT_EXECUTABLE} describe --tags --always
+	OUTPUT_VARIABLE GIT_DESC
+	ERROR_QUIET
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
 if (GIT_DESC)
 	string(STRIP ${GIT_DESC} GIT_DESC)
+	message(STATUS "Setting version to ${GIT_DESC} from git")
 else()
 	set(GIT_DESC v${CMAKE_PROJECT_VERSION})
+	message(STATUS "Setting version to ${GIT_DESC} from CMakeLists")
 endif()
 
 configure_file(${INPUT_FILE} ${OUTPUT_FILE})
