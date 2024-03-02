82ca58cbea219ad1bbd57cadcd3e65033e57f7d6
Patrick Nicolas <patricknicolas@laposte.net>
2024-03-02 19:23:39 +0100

    Drop std::pair support in serialization

--- common/version.h
+++ common/version.h
@@ -6,7 +6,7 @@
 
 namespace xrt::drivers::wivrn
 {
-	using protocol = std::pair<from_headset::packets, to_headset::packets>;
+	using protocol = std::variant<from_headset::packets, to_headset::packets>;
 
 	extern const char git_version[];
 	static inline const constinit auto protocol_version = serialization_type_hash<protocol>();
