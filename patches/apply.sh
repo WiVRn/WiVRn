8328fe13c7e7f6a2dda2204bb487f47bd642ff1f
Patrick Nicolas <patricknicolas@laposte.net>
2024-09-03 09:22:03 +0200

    Use git-am to apply monado patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+if [ -d ".git" ] ; then
+	git config user.email > /dev/null || git config user.email "git-am@invalid"
+	git config user.name > /dev/null || git config user.name "no name"
+	git checkout .
+	exec git am "$@"/*
+fi
+
 # There is no reliable way to apply a patch and succeed if it was
 # already applied. Just ignore errors...
 for f in "$@"/* ; do
