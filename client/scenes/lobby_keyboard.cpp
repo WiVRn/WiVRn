55186d2e967c4916b84dde95ae966682578fae07
Guillaume Meunier <guillaume.meunier@centraliens.net>
2024-10-18 00:15:28 +0200

    FIXME: comment out old code for virtual keyboard

--- client/scenes/lobby_keyboard.cpp
+++ client/scenes/lobby_keyboard.cpp
@@ -147,145 +147,145 @@ static std::string to_utf8(char32_t c)
 
 static bool button_behavior(const ImRect & bb, ImGuiID id, bool * out_hovered, bool * out_held, ImGuiButtonFlags flags)
 {
-	ImGuiContext & g = *GImGui;
-	ImGuiWindow * window = ImGui::GetCurrentWindow();
-
-	// Default behavior inherited from item flags
-	// Note that _both_ ButtonFlags and ItemFlags are valid sources, so copy one into the item_flags and only check that.
-	ImGuiItemFlags item_flags = (g.LastItemData.ID == id ? g.LastItemData.InFlags : g.CurrentItemFlags);
-	if (flags & ImGuiButtonFlags_AllowOverlap)
-		item_flags |= ImGuiItemFlags_AllowOverlap;
-	if (flags & ImGuiButtonFlags_Repeat)
-		item_flags |= ImGuiItemFlags_ButtonRepeat;
+	// ImGuiContext & g = *GImGui;
+	// ImGuiWindow * window = ImGui::GetCurrentWindow();
+	//
+	// // Default behavior inherited from item flags
+	// // Note that _both_ ButtonFlags and ItemFlags are valid sources, so copy one into the item_flags and only check that.
+	// ImGuiItemFlags item_flags = (g.LastItemData.ID == id ? g.LastItemData.InFlags : g.CurrentItemFlags);
+	// if (flags & ImGuiButtonFlags_AllowOverlap)
+	// 	item_flags |= ImGuiItemFlags_AllowOverlap;
+	// if (flags & ImGuiButtonFlags_Repeat)
+	// 	item_flags |= ImGuiItemFlags_ButtonRepeat;
 
 	bool pressed = false;
-	bool hovered = ImGui::ItemHoverable(bb, id, item_flags);
-
-	// Mouse handling
-	const ImGuiID test_owner_id = (flags & ImGuiButtonFlags_NoTestKeyOwner) ? ImGuiKeyOwner_Any : id;
-	if (hovered)
-	{
-		// Poll mouse buttons
-		// - 'mouse_button_clicked' is generally carried into ActiveIdMouseButton when setting ActiveId.
-		// - Technically we only need some values in one code path, but since this is gated by hovered test this is fine.
-		int mouse_button_clicked = -1;
-		int mouse_button_released = -1;
-		for (int button = 0; button < 3; button++)
-			if (flags & (ImGuiButtonFlags_MouseButtonLeft << button)) // Handle ImGuiButtonFlags_MouseButtonRight and ImGuiButtonFlags_MouseButtonMiddle here.
-			{
-				if (ImGui::IsMouseClicked(button, test_owner_id) && mouse_button_clicked == -1)
-				{
-					mouse_button_clicked = button;
-				}
-				if (ImGui::IsMouseReleased(button, test_owner_id) && mouse_button_released == -1)
-				{
-					mouse_button_released = button;
-				}
-			}
-
-		// Process initial action
-		if (!(flags & ImGuiButtonFlags_NoKeyModifiers) || (!g.IO.KeyCtrl && !g.IO.KeyShift && !g.IO.KeyAlt))
-		{
-			if (mouse_button_clicked != -1 && g.ActiveId != id)
-			{
-				if (!(flags & ImGuiButtonFlags_NoSetKeyOwner))
-					ImGui::SetKeyOwner(ImGui::MouseButtonToKey(mouse_button_clicked), id);
-				if (flags & (ImGuiButtonFlags_PressedOnClickRelease | ImGuiButtonFlags_PressedOnClickReleaseAnywhere))
-				{
-					// ImGui::SetActiveID(id, window);
-					g.ActiveIdMouseButton = mouse_button_clicked;
-					if (!(flags & ImGuiButtonFlags_NoNavFocus))
-						ImGui::SetFocusID(id, window);
-					ImGui::FocusWindow(window);
-				}
-				if ((flags & ImGuiButtonFlags_PressedOnClick) || ((flags & ImGuiButtonFlags_PressedOnDoubleClick) && g.IO.MouseClickedCount[mouse_button_clicked] == 2))
-				{
-					pressed = true;
-					// if (flags & ImGuiButtonFlags_NoHoldingActiveId)
-					//     ImGui::ClearActiveID();
-					// else
-					//     ImGui::SetActiveID(id, window); // Hold on ID
-					if (!(flags & ImGuiButtonFlags_NoNavFocus))
-						ImGui::SetFocusID(id, window);
-					g.ActiveIdMouseButton = mouse_button_clicked;
-					ImGui::FocusWindow(window);
-				}
-			}
-			if (flags & ImGuiButtonFlags_PressedOnRelease)
-			{
-				if (mouse_button_released != -1)
-				{
-					const bool has_repeated_at_least_once = (item_flags & ImGuiItemFlags_ButtonRepeat) && g.IO.MouseDownDurationPrev[mouse_button_released] >= g.IO.KeyRepeatDelay; // Repeat mode trumps on release behavior
-					if (!has_repeated_at_least_once)
-						pressed = true;
-					if (!(flags & ImGuiButtonFlags_NoNavFocus))
-						ImGui::SetFocusID(id, window);
-					// ImGui::ClearActiveID();
-				}
-			}
-
-			// 'Repeat' mode acts when held regardless of _PressedOn flags (see table above).
-			// Relies on repeat logic of IsMouseClicked() but we may as well do it ourselves if we end up exposing finer RepeatDelay/RepeatRate settings.
-			if (g.ActiveId == id && (item_flags & ImGuiItemFlags_ButtonRepeat))
-				if (g.IO.MouseDownDuration[g.ActiveIdMouseButton] > 0.0f && ImGui::IsMouseClicked(g.ActiveIdMouseButton, test_owner_id, ImGuiInputFlags_Repeat))
-					pressed = true;
-		}
-
-		if (pressed)
-			g.NavDisableHighlight = true;
-	}
-
-	// Process while held
-	bool held = false;
-	if (g.ActiveId == id)
-	{
-		if (g.ActiveIdSource == ImGuiInputSource_Mouse)
-		{
-			if (g.ActiveIdIsJustActivated)
-				g.ActiveIdClickOffset = g.IO.MousePos - bb.Min;
-
-			const int mouse_button = g.ActiveIdMouseButton;
-			if (mouse_button == -1)
-			{
-				// Fallback for the rare situation were g.ActiveId was set programmatically or from another widget (e.g. #6304).
-				// ImGui::ClearActiveID();
-			}
-			else if (ImGui::IsMouseDown(mouse_button, test_owner_id))
-			{
-				held = true;
-			}
-			else
-			{
-				bool release_in = hovered && (flags & ImGuiButtonFlags_PressedOnClickRelease) != 0;
-				bool release_anywhere = (flags & ImGuiButtonFlags_PressedOnClickReleaseAnywhere) != 0;
-				if ((release_in || release_anywhere) && !g.DragDropActive)
-				{
-					// Report as pressed when releasing the mouse (this is the most common path)
-					bool is_double_click_release = (flags & ImGuiButtonFlags_PressedOnDoubleClick) && g.IO.MouseReleased[mouse_button] && g.IO.MouseClickedLastCount[mouse_button] == 2;
-					bool is_repeating_already = (item_flags & ImGuiItemFlags_ButtonRepeat) && g.IO.MouseDownDurationPrev[mouse_button] >= g.IO.KeyRepeatDelay; // Repeat mode trumps <on release>
-					bool is_button_avail_or_owned = ImGui::TestKeyOwner(ImGui::MouseButtonToKey(mouse_button), test_owner_id);
-					if (!is_double_click_release && !is_repeating_already && is_button_avail_or_owned)
-						pressed = true;
-				}
-				// ImGui::ClearActiveID();
-			}
-			if (!(flags & ImGuiButtonFlags_NoNavFocus))
-				g.NavDisableHighlight = true;
-		}
-		else if (g.ActiveIdSource == ImGuiInputSource_Keyboard || g.ActiveIdSource == ImGuiInputSource_Gamepad)
-		{
-			// When activated using Nav, we hold on the ActiveID until activation button is released
-			// if (g.NavActivateDownId != id)
-			//     ImGui::ClearActiveID();
-		}
-		if (pressed)
-			g.ActiveIdHasBeenPressedBefore = true;
-	}
-
-	if (out_hovered)
-		*out_hovered = hovered;
-	if (out_held)
-		*out_held = held;
+	// bool hovered = ImGui::ItemHoverable(bb, id, item_flags);
+	//
+	// // Mouse handling
+	// const ImGuiID test_owner_id = (flags & ImGuiButtonFlags_NoTestKeyOwner) ? ImGuiKeyOwner_Any : id;
+	// if (hovered)
+	// {
+	// 	// Poll mouse buttons
+	// 	// - 'mouse_button_clicked' is generally carried into ActiveIdMouseButton when setting ActiveId.
+	// 	// - Technically we only need some values in one code path, but since this is gated by hovered test this is fine.
+	// 	int mouse_button_clicked = -1;
+	// 	int mouse_button_released = -1;
+	// 	for (int button = 0; button < 3; button++)
+	// 		if (flags & (ImGuiButtonFlags_MouseButtonLeft << button)) // Handle ImGuiButtonFlags_MouseButtonRight and ImGuiButtonFlags_MouseButtonMiddle here.
+	// 		{
+	// 			if (ImGui::IsMouseClicked(button, test_owner_id) && mouse_button_clicked == -1)
+	// 			{
+	// 				mouse_button_clicked = button;
+	// 			}
+	// 			if (ImGui::IsMouseReleased(button, test_owner_id) && mouse_button_released == -1)
+	// 			{
+	// 				mouse_button_released = button;
+	// 			}
+	// 		}
+	//
+	// 	// Process initial action
+	// 	if (!(flags & ImGuiButtonFlags_NoKeyModifiers) || (!g.IO.KeyCtrl && !g.IO.KeyShift && !g.IO.KeyAlt))
+	// 	{
+	// 		if (mouse_button_clicked != -1 && g.ActiveId != id)
+	// 		{
+	// 			if (!(flags & ImGuiButtonFlags_NoSetKeyOwner))
+	// 				ImGui::SetKeyOwner(ImGui::MouseButtonToKey(mouse_button_clicked), id);
+	// 			if (flags & (ImGuiButtonFlags_PressedOnClickRelease | ImGuiButtonFlags_PressedOnClickReleaseAnywhere))
+	// 			{
+	// 				// ImGui::SetActiveID(id, window);
+	// 				g.ActiveIdMouseButton = mouse_button_clicked;
+	// 				if (!(flags & ImGuiButtonFlags_NoNavFocus))
+	// 					ImGui::SetFocusID(id, window);
+	// 				ImGui::FocusWindow(window);
+	// 			}
+	// 			if ((flags & ImGuiButtonFlags_PressedOnClick) || ((flags & ImGuiButtonFlags_PressedOnDoubleClick) && g.IO.MouseClickedCount[mouse_button_clicked] == 2))
+	// 			{
+	// 				pressed = true;
+	// 				// if (flags & ImGuiButtonFlags_NoHoldingActiveId)
+	// 				//     ImGui::ClearActiveID();
+	// 				// else
+	// 				//     ImGui::SetActiveID(id, window); // Hold on ID
+	// 				if (!(flags & ImGuiButtonFlags_NoNavFocus))
+	// 					ImGui::SetFocusID(id, window);
+	// 				g.ActiveIdMouseButton = mouse_button_clicked;
+	// 				ImGui::FocusWindow(window);
+	// 			}
+	// 		}
+	// 		if (flags & ImGuiButtonFlags_PressedOnRelease)
+	// 		{
+	// 			if (mouse_button_released != -1)
+	// 			{
+	// 				const bool has_repeated_at_least_once = (item_flags & ImGuiItemFlags_ButtonRepeat) && g.IO.MouseDownDurationPrev[mouse_button_released] >= g.IO.KeyRepeatDelay; // Repeat mode trumps on release behavior
+	// 				if (!has_repeated_at_least_once)
+	// 					pressed = true;
+	// 				if (!(flags & ImGuiButtonFlags_NoNavFocus))
+	// 					ImGui::SetFocusID(id, window);
+	// 				// ImGui::ClearActiveID();
+	// 			}
+	// 		}
+	//
+	// 		// 'Repeat' mode acts when held regardless of _PressedOn flags (see table above).
+	// 		// Relies on repeat logic of IsMouseClicked() but we may as well do it ourselves if we end up exposing finer RepeatDelay/RepeatRate settings.
+	// 		if (g.ActiveId == id && (item_flags & ImGuiItemFlags_ButtonRepeat))
+	// 			if (g.IO.MouseDownDuration[g.ActiveIdMouseButton] > 0.0f && ImGui::IsMouseClicked(g.ActiveIdMouseButton, test_owner_id, ImGuiInputFlags_Repeat))
+	// 				pressed = true;
+	// 	}
+	//
+	// 	if (pressed)
+	// 		g.NavDisableHighlight = true;
+	// }
+	//
+	// // Process while held
+	// bool held = false;
+	// if (g.ActiveId == id)
+	// {
+	// 	if (g.ActiveIdSource == ImGuiInputSource_Mouse)
+	// 	{
+	// 		if (g.ActiveIdIsJustActivated)
+	// 			g.ActiveIdClickOffset = g.IO.MousePos - bb.Min;
+	//
+	// 		const int mouse_button = g.ActiveIdMouseButton;
+	// 		if (mouse_button == -1)
+	// 		{
+	// 			// Fallback for the rare situation were g.ActiveId was set programmatically or from another widget (e.g. #6304).
+	// 			// ImGui::ClearActiveID();
+	// 		}
+	// 		else if (ImGui::IsMouseDown(mouse_button, test_owner_id))
+	// 		{
+	// 			held = true;
+	// 		}
+	// 		else
+	// 		{
+	// 			bool release_in = hovered && (flags & ImGuiButtonFlags_PressedOnClickRelease) != 0;
+	// 			bool release_anywhere = (flags & ImGuiButtonFlags_PressedOnClickReleaseAnywhere) != 0;
+	// 			if ((release_in || release_anywhere) && !g.DragDropActive)
+	// 			{
+	// 				// Report as pressed when releasing the mouse (this is the most common path)
+	// 				bool is_double_click_release = (flags & ImGuiButtonFlags_PressedOnDoubleClick) && g.IO.MouseReleased[mouse_button] && g.IO.MouseClickedLastCount[mouse_button] == 2;
+	// 				bool is_repeating_already = (item_flags & ImGuiItemFlags_ButtonRepeat) && g.IO.MouseDownDurationPrev[mouse_button] >= g.IO.KeyRepeatDelay; // Repeat mode trumps <on release>
+	// 				bool is_button_avail_or_owned = ImGui::TestKeyOwner(ImGui::MouseButtonToKey(mouse_button), test_owner_id);
+	// 				if (!is_double_click_release && !is_repeating_already && is_button_avail_or_owned)
+	// 					pressed = true;
+	// 			}
+	// 			// ImGui::ClearActiveID();
+	// 		}
+	// 		if (!(flags & ImGuiButtonFlags_NoNavFocus))
+	// 			g.NavDisableHighlight = true;
+	// 	}
+	// 	else if (g.ActiveIdSource == ImGuiInputSource_Keyboard || g.ActiveIdSource == ImGuiInputSource_Gamepad)
+	// 	{
+	// 		// When activated using Nav, we hold on the ActiveID until activation button is released
+	// 		// if (g.NavActivateDownId != id)
+	// 		//     ImGui::ClearActiveID();
+	// 	}
+	// 	if (pressed)
+	// 		g.ActiveIdHasBeenPressedBefore = true;
+	// }
+	//
+	// if (out_hovered)
+	// 	*out_hovered = hovered;
+	// if (out_held)
+	// 	*out_held = held;
 
 	return pressed;
 }
