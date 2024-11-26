1e3367d5f2a358bf066f739ea34cf355dafe0720
Patrick Nicolas <patricknicolas@laposte.net>
2024-11-26 23:35:54 +0100

    fix locale check file order

--- tools/update_messages.sh
+++ tools/update_messages.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/..
 
 echo Generating template
 WIVRN_POT=$(mktemp)
-xgettext --keyword=_:1,1t --keyword=_S:1,1t --keyword=_F:1,1t $(find client/ -name "*.cpp") --output=$WIVRN_POT --package-name=WiVRn
+xgettext --keyword=_:1,1t --keyword=_S:1,1t --keyword=_F:1,1t $(find client/ -name "*.cpp" | sort) --output=$WIVRN_POT --package-name=WiVRn
 sed -i 's/charset=CHARSET/charset=UTF-8/g' $WIVRN_POT
 
 for i in $(find client/locale -name wivrn.po)
