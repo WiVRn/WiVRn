c4f1c6304ee7952ec93131e61e7f13befff0ca96
stella <crusher5000@outlook.com>
2025-03-24 23:47:14 +0800

    Initial thumb offset for meta headsets handtracking

--- client/xr/hand_tracker.cpp
+++ client/xr/hand_tracker.cpp
@@ -22,6 +22,12 @@
 #include "xr/instance.h"
 #include "xr/session.h"
 #include <cassert>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.inl>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include "hardware.h"
 
 static PFN_xrDestroyHandTrackerEXT xrDestroyHandTrackerEXT{};
 
@@ -32,11 +38,24 @@ XrResult xr::destroy_hand_tracker(XrHandTrackerEXT id)
 
 xr::hand_tracker::hand_tracker(instance & inst, session & session, const XrHandTrackerCreateInfoEXT & info)
 {
+	hand_id = info.hand;
+	
+	if (hand_id == XrHandEXT::XR_HAND_LEFT_EXT)
+	{
+		offset_angle = glm::radians(-90.0f);
+	}
+	if (hand_id == XrHandEXT::XR_HAND_RIGHT_EXT)
+	{
+		offset_angle= glm::radians(90.0f);
+	}
+
 	static auto xrCreateHandTrackerEXT = inst.get_proc<PFN_xrCreateHandTrackerEXT>("xrCreateHandTrackerEXT");
 	assert(xrCreateHandTrackerEXT);
 	xrLocateHandJointsEXT = inst.get_proc<PFN_xrLocateHandJointsEXT>("xrLocateHandJointsEXT");
 	xrDestroyHandTrackerEXT = inst.get_proc<PFN_xrDestroyHandTrackerEXT>("xrDestroyHandTrackerEXT");
 	CHECK_XR(xrCreateHandTrackerEXT(session, &info, &id));
+
+
 }
 
 std::optional<std::array<xr::hand_tracker::joint, XR_HAND_JOINT_COUNT_EXT>> xr::hand_tracker::locate(XrSpace space, XrTime time)
@@ -76,7 +95,48 @@ std::optional<std::array<xr::hand_tracker::joint, XR_HAND_JOINT_COUNT_EXT>> xr::
 	std::array<xr::hand_tracker::joint, XR_HAND_JOINT_COUNT_EXT> joints;
 	for (int i = 0; i < XR_HAND_JOINT_COUNT_EXT; i++)
 	{
-		joints[i] = {joints_pos[i], joints_vel[i]};
+		switch (guess_model())
+		{
+			case model::meta_quest_3:
+			case model::meta_quest_pro:
+			case model::meta_quest_3s:
+				if (hand_id == XR_HAND_LEFT_EXT)
+					offset_angle = glm::radians(-90.0f);
+				else if (hand_id == XR_HAND_RIGHT_EXT)
+					offset_angle = glm::radians(90.0f);
+				break;
+			
+			default:
+				offset_angle = 0.0f;
+				break;
+		}
+
+		if (i >= XR_HAND_JOINT_THUMB_METACARPAL_EXT && i <= XR_HAND_JOINT_THUMB_TIP_EXT)
+		{
+
+			// Need to convert the XrQuaternionf to a glm::quat to use glm::rotate
+			glm::quat q(
+				joints_pos[i].pose.orientation.w,
+				joints_pos[i].pose.orientation.x,
+				joints_pos[i].pose.orientation.y,
+				joints_pos[i].pose.orientation.z);
+
+			glm::quat offset_rotation = glm::rotate(q,offset_angle, glm::vec3(0,0,1));
+
+			joints_pos[i].pose.orientation = {
+				.x = offset_rotation.x,
+				.y = offset_rotation.y,
+				.z = offset_rotation.z,
+				.w = offset_rotation.w,
+			};
+
+			joints[i] = {joints_pos[i], joints_vel[i]};
+		}
+		else
+		{
+			joints[i] = {joints_pos[i], joints_vel[i]};
+		}
+
 	}
 
 	return joints;
