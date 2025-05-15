3ed4acfcf47b3ab460c5244f1f333bb4f377f6b8
Sapphire <imsapphire0@gmail.com>
2025-05-15 11:46:08 -0500

    clang-format

--- common/xr/pico_eye_types_reflection.h
+++ common/xr/pico_eye_types_reflection.h
@@ -19,13 +19,13 @@
 // See https://pico.crx.moe/docs/pico-openxr/eye-gaze-interaction and https://pico.crx.moe/docs/pico-openxr/eye-tracking.
 
 #define XR_LIST_ENUM_XrTrackingStateCodePICO(_) \
-	_(XR_MT_SUCCESS_PICO,  0) \
-	_(XR_MT_FAILURE_PICO, -1) \
-	_(XR_MT_MODE_NONE_PICO, -2) \
-	_(XR_MT_DEVICE_NOT_SUPPORT_PICO, -3) \
-	_(XR_MT_SERVICE_NEED_START_PICO, -4) \
-	_(XR_MT_ET_PERMISSION_DENIED_PICO, -5) \
-	_(XR_MT_FT_PERMISSION_DENIED_PICO, -6) \
+	_(XR_MT_SUCCESS_PICO, 0)                \
+	_(XR_MT_FAILURE_PICO, -1)               \
+	_(XR_MT_MODE_NONE_PICO, -2)             \
+	_(XR_MT_DEVICE_NOT_SUPPORT_PICO, -3)    \
+	_(XR_MT_SERVICE_NEED_START_PICO, -4)    \
+	_(XR_MT_ET_PERMISSION_DENIED_PICO, -5)  \
+	_(XR_MT_FT_PERMISSION_DENIED_PICO, -6)  \
 	_(XR_MT_MIC_PERMISSION_DENIED_PICO, -7) \
-	_(XR_MT_SYSTEM_DENIED_PICO, -8) \
+	_(XR_MT_SYSTEM_DENIED_PICO, -8)         \
 	_(XR_MT_UNKNOWN_ERROR_PICO, -9)
