00e1548dfb2a14c4661ddbb08ef80e249e14d742
Sapphire <imsapphire0@gmail.com>
2025-06-04 13:48:03 -0500

    fixup! Use separate packet to send generic tracker poses
    fix space relation tracked bits

--- client/xr/fb_body_tracker.cpp
+++ client/xr/fb_body_tracker.cpp
@@ -118,13 +118,13 @@ std::optional<std::array<wivrn::from_headset::body_tracking::pose, wivrn::from_h
 		};
 
 		if (joint_pose.locationFlags & XR_SPACE_LOCATION_ORIENTATION_VALID_BIT)
-			pose.flags |= wivrn::from_headset::tracking::orientation_valid;
+			pose.flags |= wivrn::from_headset::body_tracking::orientation_valid;
 		if (joint_pose.locationFlags & XR_SPACE_LOCATION_POSITION_VALID_BIT)
-			pose.flags |= wivrn::from_headset::tracking::position_valid;
+			pose.flags |= wivrn::from_headset::body_tracking::position_valid;
 		if (joint_pose.locationFlags & XR_SPACE_LOCATION_ORIENTATION_TRACKED_BIT)
-			pose.flags |= wivrn::from_headset::tracking::orientation_tracked;
+			pose.flags |= wivrn::from_headset::body_tracking::orientation_tracked;
 		if (joint_pose.locationFlags & XR_SPACE_LOCATION_POSITION_TRACKED_BIT)
-			pose.flags |= wivrn::from_headset::tracking::position_tracked;
+			pose.flags |= wivrn::from_headset::body_tracking::position_tracked;
 
 		poses[num_poses++] = std::move(pose);
 	}
