230e102f36a7739bb6186e3b9c383c2d0e5a5ed8
Guillaume Meunier <guillaume.meunier@centraliens.net>
2025-08-08 19:55:17 +0200

    Only request the app list when the stream is stalled

--- client/scenes/stream.cpp
+++ client/scenes/stream.cpp
@@ -316,15 +316,6 @@ std::shared_ptr<scenes::stream> scenes::stream::create(std::unique_ptr<wivrn_ses
 	}
 
 	{
-		const auto & info = application::get_messages_info();
-		self->network_session->send_control(from_headset::get_application_list{
-		        .language = info.language,
-		        .country = info.country,
-		        .variant = info.variant,
-		});
-	}
-
-	{
 		const auto & config = application::get_config();
 		self->override_foveation_enable = config.override_foveation_enable;
 		self->override_foveation_pitch = config.override_foveation_pitch;
@@ -770,7 +761,15 @@ void scenes::stream::render(const XrFrameState & frame_state)
 	}
 
 	if (state_ == state::stalled)
+	{
+		network_session->send_control(from_headset::get_application_list{
+		        .language = application::get_messages_info().language,
+		        .country = application::get_messages_info().country,
+		        .variant = application::get_messages_info().variant,
+		});
+
 		application::pop_scene();
+	}
 
 	assert(swapchain);
 	for (auto & i: decoders)
