869911fcd50c91f28cf781c5d982b718623ff52f
Patrick Nicolas <patricknicolas@laposte.net>
2025-08-10 14:19:01 +0200

    pythonize check_messages (merged into check_po)

--- tools/check_po.py
+++ tools/check_po.py
@@ -3,49 +3,103 @@
 import sys
 import polib
 import argparse
+import os
 import pathlib
+import subprocess
+import tempfile
 
-parser = argparse.ArgumentParser(
-	prog='check_po.py',
-	description='Check that all strings are translated in a .po file')
+def list_files(base: str, exts: str|list[str]):
+	if type(exts) == str:
+		exts = [exts]
+	for root, dirs, files in os.walk(base):
+		for f in files:
+			for ext in exts:
+				if f.endswith(ext):
+					yield os.path.join(root, f)
 
-parser.add_argument('ref', type=pathlib.Path)
-parser.add_argument('file', type=pathlib.Path)
-parser.add_argument('lang', type=str)
+def flag(lang: str):
+	# Map language code to a country flag
+	countries = {
+		'es': 'es',
+		'fr': 'fr',
+		'it': 'it',
+		'ja': 'jp',
+		'zh_TW': 'tw',
+		'pt_BR': 'br'
+	}
 
-args = parser.parse_args()
+	country = countries.get(lang, lang)
 
-po_ref = polib.pofile(args.ref)
+	res = ''
+	for i in country.upper():
+		res += chr(ord(i) + ord('🇦') - ord('A'))
+	return res
 
-po_file = polib.pofile(args.file)
-entries = [i.msgid for i in po_file.translated_entries()]
+if __name__ == "__main__":
+	root = os.path.dirname(os.path.dirname(__file__))
+	locale_dir = os.path.join(root, "locale")
+	cwd = os.getcwd()
 
-# Map language code to a country flag
-countries = {
-	'es': 'es',
-	'fr': 'fr',
-	'it': 'it',
-	'ja': 'jp',
-	'zh_TW': 'tw',
-	'pt_BR': 'br'
-}
+	parser = argparse.ArgumentParser(
+		prog='check_po.py',
+		description='Check that all strings are translated in a .po file')
 
-if args.lang in countries:
-	country = countries[args.lang]
-else:
-	country = args.lang
+	parser.add_argument('lang', type=str, nargs="*")
 
-flag = ''
-for i in country.upper():
-	flag += chr(ord(i) + ord('🇦') - ord('A'))
+	args = parser.parse_args()
 
-missing = 0
-for i in po_ref:
-	if not i.obsolete:
-		if not i.msgid in entries:
-			missing = missing + 1
-			print(f"::notice file={args.file}::{flag} Translation for {repr(i.msgid)} is missing")
+	if len(args.lang) == 0:
+		for d in sorted(os.listdir(locale_dir)):
+			if os.path.isdir(os.path.join(locale_dir, d)):
+				args.lang.append(d)
 
-if missing > 0:
-	print(f"::warning file={args.file}::{flag} {missing} translations missing")
-	sys.exit(1)
+	with tempfile.TemporaryDirectory() as tmpdir:
+		client_pot = os.path.join(tmpdir, "client.pot")
+		dashboard_pot = os.path.join(tmpdir, "dashboard.pot")
+
+		subprocess.check_call(
+				["xgettext",
+				"--c++", "--from-code=UTF-8",
+				"--keyword=_:1,1t",
+				"--keyword=_S:1,1t",
+				"--keyword=_cS:1c,2,2t",
+				"--keyword=_F:1,1t",
+				"--output", client_pot,
+				"--package-name=WiVRn",
+				] + sorted(list_files(os.path.join(root, "client"), ".cpp"))
+				)
+
+		subprocess.check_call(
+				["xgettext",
+				"--c++", "--kde", "--from-code=UTF-8",
+				"--keyword=i18n:1",
+				"--keyword=i18nc:1c,2",
+				"--keyword=i18np:1,2",
+				"--keyword=i18ncp:1c,2,3",
+				"--output", dashboard_pot,
+				"--package-name=WiVRn-dashboard",
+				] + sorted(list_files(os.path.join(root, "dashboard"), [".cpp", ".qml"]))
+				)
+
+		client_ref = polib.pofile(client_pot)
+		dashboard_ref = polib.pofile(dashboard_pot)
+
+		for lang in args.lang:
+
+			for po_ref, po in ((client_ref, os.path.join(locale_dir, lang, "wivrn.po")),
+				   (dashboard_ref, os.path.join(locale_dir, lang, "wivrn-dashboard.po"))):
+				if not os.path.exists(po):
+					continue
+				po_file = polib.pofile(po)
+				po = os.path.relpath(po, cwd)
+				entries = [i.msgid for i in po_file.translated_entries()]
+
+				missing = 0
+				for i in po_ref:
+					if not i.obsolete:
+						if not i.msgid in entries:
+							missing = missing + 1
+							print(f"::notice file={po}::{flag(lang)} Translation for {repr(i.msgid)} is missing")
+
+				if missing > 0:
+					print(f"::warning file={po}::{flag(lang)} {missing} translations missing")
