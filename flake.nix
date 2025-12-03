ddea4eac5a027fb7dc867ae5cc9b76d03eea3729
Sapphire <imsapphire0@gmail.com>
2025-12-03 17:25:32 -0600

    Update monado

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
               # Keep in sync with CMakeLists.txt monado rev
               rev = builtins.readFile ./monado-rev;
               # Nix will output the correct hash when it doesn't match
-              hash = "sha256-KNVC9zKPEDzuXvkzvPqexpfHJ7rG3PUuIhyCPNz3dhg=";
+              hash = "sha256-Saspj7H9XIM/meo9OykdK5rAbXqgrORgrH+CdphczUk=";
             };
           };
 
